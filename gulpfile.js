cfe02efafb1590c49e7fd246db11610c98f929d5
dave-hawkins <dave@branch-creative.co.uk>
2016-07-27 11:48:01 +0100

    changed deploy message

--- gulpfile.js
+++ gulpfile.js
@@ -72,6 +72,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src('./_site/**/*')
     .pipe(ghPages());
 });
